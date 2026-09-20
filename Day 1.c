fe0ac30b9abb297a30d84763a148d35f46a8d820
Bansal1710 <Bhuvnesh.39788@stu.upes.ac.in>
2026-09-20 17:19:14 +0530

    Update Day 1.c

--- Day 1.c
+++ Day 1.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
+    int num1, num2;
 
-    printf("Enter two numbers: \n");
+    printf("Enter two numbers:\n");
     scanf("%d %d", &num1, &num2);
 
-    sum = num1 + num2;
+    printf("Sum = %d\n", num1 + num2);
+    printf("Difference = %d\n", num1 - num2);
+    printf("Product = %d\n", num1 * num2);
 
-    printf("Sum = %d\n", sum);
+    if (num2 != 0)
+        printf("Quotient = %.2f\n", (float)num1 / num2);
+    else
+        printf("Quotient = Cannot divide by zero\n");
 
     return 0;
 }
