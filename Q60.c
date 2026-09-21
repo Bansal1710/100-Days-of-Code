fac2bcddc87dea5fae6f248c61ebf392109b6e64
Bansal1710 <Bhuvnesh.39788@stu.upes.ac.in>
2026-09-21 18:54:36 +0530

    Create Q60.c

---
+++ Q60.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a[5], i;
+    int positive = 0, negative = 0, zero = 0;
+
+    printf("Enter 5 elements:\n");
+
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < 5; i++) {
+        if (a[i] > 0) {
+            positive++;
+        } 
+        else if (a[i] < 0) {
+            negative++;
+        } 
+        else {
+            zero++;
+        }
+    }
+
+    printf("Positive = %d\n", positive);
+    printf("Negative = %d\n", negative);
+    printf("Zero = %d\n", zero);
+
+    return 0;
+}
