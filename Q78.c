52cf49051341338a3a79372c0f978c8e946ad450
Bansal1710 <Bhuvnesh.39788@stu.upes.ac.in>
2026-09-21 19:13:38 +0530

    Create Q78.c

---
+++ Q78.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j, sum = 0;
+
+    printf("Enter order of matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter matrix elements:\n");
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Sum of main diagonal
+    for (i = 0; i < n; i++) {
+        sum = sum + a[i][i];
+    }
+
+    printf("Sum of main diagonal = %d\n", sum);
+
+    return 0;
+}
